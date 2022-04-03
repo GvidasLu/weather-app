267b6e088ff6961303089d4b077710519f811ee7
GvidasLu <luspinis.gvidas@gmail.com>
2022-04-03 23:23:53 +0300

    added info card section; added new components files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
-  const API_KEY = process.env.REACT_APP_API_KEY
-
-  const [noData, setNoData] = useState('No Data Yet')
-  const [searchTerm, setSearchTerm] = useState('')
-  const [weatherData, setWeatherData] = useState([])
-  const [city, setCity] = useState('Unknown location')
-  const [weatherIcon, setWeatherIcon] = useState(`${process.env.REACT_APP_ICON_URL}10@2x.png`)
+  const [noData, setNoData] = useState("No Data Yet");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+  const [city, setCity] = useState("Unknown location");
+  const [weatherIcon, setWeatherIcon] = useState(
+    `${process.env.REACT_APP_ICON_URL}10@2x.png`
+  );
 
   return (
     <div className="bg-gray-800 flex items-center justify-center w-screen h-screen py-10">
@@ -28,21 +28,30 @@ function App() {
             </div>
           </div>
           <div className="flex flex-col items-center justify-center h-full">
-            <h1 className="text-white text-2x1">The Only Weather Forecast You Need</h1>
+            <h1 className="text-white text-2x1">
+              The Only Weather Forecast You Need
+            </h1>
             <hr className="h-1 bg-white w-1/4 rounded-full my-5" />
-            <form noValidate onSubmit={handleSubmit} className="flex justify-center w-full">
-              <input 
-               type="text"
-               className="relative rounded-x1 py-2 px-3 w-2/3 bg-gray-300 bg-opacity-60 text-white placeholder-gray-200"
-               onChange={handleChange}
-               required/>
-               <button type="submit" className="z-10">
-                 <i className="fa fa-search text-white m1-10 border-1 my-auto z-10 cursor-pointer p-3"
+            <form
+              noValidate
+              onSubmit={handleSubmit}
+              className="flex justify-center w-full"
+            >
+              <input
+                type="text"
+                className="relative rounded-x1 py-2 px-3 w-2/3 bg-gray-300 bg-opacity-60 text-white placeholder-gray-200"
+                onChange={handleChange}
+                required
+              />
+              <button type="submit" className="z-10">
+                <i
+                  className="fa fa-search text-white m1-10 border-1 my-auto z-10 cursor-pointer p-3"
                   aria-hidden="true"
                   onClick={() => {
-                    navigator.geolocation.getCurrentPosition(myIP)
-                  }}></i>
-               </button>
+                    navigator.geolocation.getCurrentPosition(myIP);
+                  }}
+                ></i>
+              </button>
             </form>
           </div>
         </div>
@@ -51,9 +60,28 @@ function App() {
         <div className="w-2/4 p-5">
           <Header />
           <div className="flex flex-col my-10">
-            {weatherData.length === 0 ?
-              <div></div> : <></>
-            }
+            {weatherData.length === 0 ? (
+              <div className="container p-4 flex items-center justify-center h-1/3 mb-auto">
+                <h1 className="text-gray-300 text-4x1 font-bold uppercase">
+                  {noData}
+                </h1>
+              </div>
+            ) : (
+              <>
+                <h1 className="text-5x1 text-gray-800 mt-auto mb-4">Today</h1>
+                <DetailCard weather_icon={weatherIcon} data={weatherData} />
+                <h1 className="text-3x1 text-gray-600 mb-4 mt-10">
+                  More On {City}
+                </h1>
+                <ul className="grid grid-cols-2 gap-2">
+                  {weatherData.list.map((days, index) => {
+                    if (index > 0) {
+                      return <SummaryCard key={index} day={days} />;
+                    }
+                  })}
+                </ul>
+              </>
+            )}
           </div>
         </div>
       </div>
